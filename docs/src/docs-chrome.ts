a954e145924bc7b16aa6fa705d71edebe18fa08b
Liz Ainslie <lizzy@lizainslie.dev>
2026-09-15 19:34:58 +0000

    docs: chrome config

---
+++ docs/src/docs-chrome.ts
@@ -0,0 +1,10 @@
+import { defineDocsChrome } from '@kolektiv/common-docs-chrome';
+
+export const docs = defineDocsChrome({
+  product: 'Kascade',
+  siteUrl: 'https://kascade.kolektiv.computer',
+  base: '/',
+  nav: [
+    { label: 'Getting started', href: '/docs/' },
+  ],
+});
